f4f6f76e5e3c61d5eedb87c71687e54fe330ec2e
GitLqr <960432662@qq.com>
2020-12-19 15:40:22 +0800

    实现商品分类分页及热销产品数据查询.

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name='list'),
+    url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,13 +1,30 @@
+from django import http
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
-
 # Create your views here.
 from django.views import View
-from django import http
 
 from contents.utils import get_categories
-from goods.models import GoodsCategory
+from goods.models import GoodsCategory, SKU
 from goods.utils import get_breadcrumb
-from django.core.paginator import Paginator
+from meiduo_mall.utils.response_code import RETCODE
+
+
+class HotGoodsView(View):
+    """热销排行"""
+
+    def get(self, request, category_id):
+        skus = SKU.objects.filter(category_id=category_id, is_launched=True).order_by('-sales')[:2]
+        hot_skus = []
+        for sku in skus:
+            sku_dict = {
+                'id': sku.id,
+                'name': sku.name,
+                'price': sku.price,
+                'default_image_url': sku.default_image.url
+            }
+            hot_skus.append(sku_dict)
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'hot_skus': hot_skus})
 
 
 class ListView(View):
@@ -40,13 +57,21 @@ class ListView(View):
         skus = category.sku_set.filter(is_launched=True).order_by(sort_field)
 
         # 创建分页器
-        paginator = Paginator(skus, 5)  # 把skus进行分页, 每页5条记录
+        try:
+            paginator = Paginator(skus, 5)  # 把skus进行分页, 每页5条记录
+        except EmptyPage:
+            return http.HttpResponseNotFound('Empty Page')
         page_skus = paginator.page(page_num)  # 获取到用户当前要看的那一页
         total_page = paginator.num_pages  # 获取总页数
 
         context = {
             'categories': categories,
-            'breadcrumb': breadcrumb
+            'breadcrumb': breadcrumb,
+            'page_skus': page_skus,
+            'total_page': total_page,
+            'page_num': page_num,
+            'sort': sort,
+            'category_id': category_id,
         }
 
         return render(request, 'list.html', context)
